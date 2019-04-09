ce1a0742c4bf27b2f4a5405798fdf403ace4dcb7
Jonatan <jonatan.karlber@ha.ax>
2019-04-09 11:23:16 +0300

    added skeleton to module_CMPS12

--- Modules/module_CMPS12.hpp
+++ Modules/module_CMPS12.hpp
@@ -1,12 +1,27 @@
 #ifndef __MODULE_CMPS12_HPP__
 #define  __MODULE_CMPS12_HPP__
+#include <vector>
 class Module_CMPS12
 {
 
     private:
+      //Hardware component for our compass module
+      CMPS12                    m_CMPS12_hardware_connection;
+
+      //Container for X amount of stored data-pulls
+      std::vector<CMPS12_Data>  m_CMPS12_data_set;
+
+      //Is this module initialized
+      bool                      m_active;
 
     public:
       Module_CMPS12();
+      ~Module_CMPS12();
+      void init();
+      void run();
+
+      //For Eventual Observer request
+      //bool send();
 
 };
 #endif//__MODULE_CMPS12_HPP__
