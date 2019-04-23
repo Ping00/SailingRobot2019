45208b55ceee54afe57db4b46d63a043ed9db970
Jonatan <jonatan.karlber@ha.ax>
2019-04-23 11:12:33 +0300

    added conversion from two 2d coordinate systems

--- Modules/Calculation_Unit/calculation_unit.cpp
+++ Modules/Calculation_Unit/calculation_unit.cpp
@@ -12,13 +12,26 @@ double Calculation_Unit::radians_to_degrees(double radians)
 
 double Calculation_Unit::convert_coordinates(double from_low, double from_high, double to_low, double to_high, double position)
 {
-    return 0;
-}
 
-VEC2 radians_to_vector(double value)
-{
-    VEC2 temp;
-    return temp;
+    double percentile = (position - from_low) / (from_high - from_low);
+    int result = percentile*(to_high - to_low )+to_low;
+    std::cout << "PERCENTILE: " << percentile << std::endl;
+    std::cout << "RESULT: " << result << std::endl;
+
+    /*
+  int servo_lower_limit = m_servo_hardware_connection.get_lower_limit();
+	int servo_upper_limit = m_servo_hardware_connection.get_upper_limit();
+
+	std::cout << "TARGET IS: " << m_target << std::endl;
+
+	double percentile = (m_target - m_lower_boundary) / (m_upper_boundary - m_lower_boundary);
+
+	std::cout << "PERCENTILE IS: " << percentile << std::endl;
+
+	int destination = percentile*(servo_upper_limit-servo_lower_limit)+servo_lower_limit;
+  */
+
+    return 0;
 }
 
 double Calculation_Unit::calculate_rudder_position(VEC2)
@@ -36,8 +49,13 @@ double Calculation_Unit::calculate_sail_position(VEC2)
     return 0;
 }
 
-double  vector_to_degrees(VEC2 vector)
+VEC2    Calculation_Unit::degrees_to_vector(double value)
 {
+    VEC2 temp;
+    return temp;
+}
 
+double  Calculation_Unit::vector_to_degrees(VEC2 vector)
+{
     return 0;
 }
