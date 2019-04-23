64b0662d7e80a9eb1fab188c8da43f8e113ad316
Jonatan <jonatan.karlber@ha.ax>
2019-04-23 08:23:37 +0300

    renamed file

---
+++ Modules/Calculation_Unit/calculation_unit.cpp
@@ -0,0 +1,1 @@
+#include "calculation_unit.hpp"
---
+++ Modules/Calculation_Unit/calculation_unit.hpp
@@ -0,0 +1,14 @@
+#ifndef __CALCULATION_UNIT_HPP__
+#define __CALCULATION_UNIT_HPP__
+class Calculation_Unit
+{
+    private:
+
+
+    public:
+      double degrees_to_radians(double degrees);
+      double radians_to_degrees(double radians);
+
+
+};
+#endif//__CALCULATION_UNIT_HPP__
