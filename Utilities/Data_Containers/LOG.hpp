19f02c68e3bf2db38af96d60b06c790087d82fcb
Jonatan <jonatan.karlber@ha.ax>
2019-04-30 08:41:19 +0300

    added members to log

---
+++ Utilities/Data_Containers/LOG.hpp
@@ -0,0 +1,18 @@
+#ifndef __LOG_HPP__
+#define __LOG_HPP__
+#include <string
+class Log
+{
+    private:
+
+    public:
+        Log();
+        
+        double          m_bearing;
+        double          m_latitude;
+        double          m_longitude;
+        double          m_speed;
+        std::string     m_timestamp;
+
+}
+#endif//__LOG_HPP__
