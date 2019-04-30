0a6c2a89224ca549a873ecbf092f56e47d029c20
Jonatan <jonatan.karlber@ha.ax>
2019-04-30 08:38:04 +0300

    added logger functions

--- Modules/Logger/logger.hpp
+++ Modules/Logger/logger.hpp
@@ -1,11 +1,18 @@
 #ifndef __LOGGER_HPP__
 #define __LOGGER_HPP__
+#include <vector>
 class Logger
 {
     private:
-        
+        int              m_entries;
+        std::vector<LOG> m_log_entries;
     
     public:
         Logger();
+        
+        void                log(LOG packet);
+        int                 get_amount_of_entries();
+        LOG                 get_entry(int id);
+        std::vector<LOG>    get_all_logs();
 }
 #endif//__LOGGER_HPP__
