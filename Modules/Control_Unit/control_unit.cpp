9c0b0c49cd6dcae9ab4ca1303fe1f7687093b6d4
Jonatan <jonatan.karlber@ha.ax>
2019-05-14 12:23:18 +0300

    working out fringe cases in initialization section

--- Modules/Control_Unit/control_unit.cpp
+++ Modules/Control_Unit/control_unit.cpp
@@ -22,9 +22,56 @@ bool Control_Unit::init(std::string destination, std::string settings)
     IO io;
     Parser parser;
 
-    std::vector<std::string> destination_data = io.read_file(destination);
-    std::cout << "Lines: " << destination_data.size() << std::endl;
+    //Get file data and remove all comments
+    std::vector<std::string> data_raw = io.read_file(destination);
+    std::vector<std::string> data_clean = parser.remove_comments(data_raw);
+    int checkpoints = data_clean.size();
+    std::cout << "We Have #" << checkpoints << " Checkpoints to travel to" << std::endl;
 
+    //Store each checkpoints in reverse (FIFO, we want our first one to be on top of the stack)
+    for(int i = checkpoints; i > 0; i--)
+    {
+        std::vector<std::string> split = parser.split_string(data_clean[i-1],',');
+        if(split.size() == 2)
+        {
+            GPS_POSITION pin;
+            pin.latitude  = std::atof(split[0].c_str());
+            pin.longitude = std::atof(split[1].c_str());
+            m_destination.push(pin);
+        }
+        else
+        {
+            std::cout << "COORDINATE ERROR" << std::endl;
+            m_active = false;
+            return false;
+        }
+    }
+    std::cout << "Destination Coordinates Logged" << std::endl;
+    m_destination_set = true;
+
+    //Store threshold variables
+    std::vector<std::string> settings_raw = io.read_file(settings);
+    std::vector<std::string> settings_clean = parser.remove_comments(settings_raw);
+    //NOTE THAT OUR ELEMENTS ARE THE SETTINGS POSITIONS; REMOVAL OF SETTINGS OR
+    //ADDING WITHOUT MENDING WILL CAUSE FAULTS
+    int total_settings = settings_clean.size();
+    std::cout << "Total Settings found: " << total_settings << std::endl;
+    if(total_settings < 2)
+    {
+        std::cout << "SETTINGS FILE CORRUPT / MISSING: DEFAULTING TO STANDARD VALUES (0/0)" << std::endl;
+        m_distance_threshold = 0;
+        m_time_threshold = 0;
+    }
+    else
+    {
+          m_distance_threshold = std::atof(settings_clean[0].c_str());
+          m_time_threshold = std::atof(settings_clean[1].c_str());
+    }
+
+    std::cout << "Distance (M) : " << m_distance_threshold << std::endl;
+    std::cout << "Time     (S) : " << m_time_threshold << std::endl;
+
+    m_active = true;
     return true;
 }
 
--- Modules/Control_Unit/control_unit.hpp
+++ Modules/Control_Unit/control_unit.hpp
@@ -8,35 +8,35 @@ class Control_Unit
 {
     private:
 
-        bool          m_active;                     //Is the Control Unit Activated
-        bool          m_waypoint_reached;           //Have we reached our waypoint
-        bool          m_waypoint_set;               //Have we set a waypoint?
-        bool          m_destination_set;            //Have we set a destination
-        bool          m_destination_reached;        //Have we reached our destination
+        bool                      m_active;                     //Is the Control Unit Activated
+        bool                      m_waypoint_reached;           //Have we reached our waypoint
+        bool                      m_waypoint_set;               //Have we set a waypoint?
+        bool                      m_destination_set;            //Have we set a destination
+        bool                      m_destination_reached;        //Have we reached our destination
 
-        std::queue<GPS_POSITION>  m_destination;    //Queue of all our destinations
-        GPS_POSITION              m_waypoint;       //Our current waypoint
+        std::queue<GPS_POSITION>  m_destination;                //Queue of all our destinations
+        GPS_POSITION              m_waypoint;                   //Our current waypoint
 
-        double        m_distance_threshold;         //The distance we are allowed to be away from the waypoint to consider it reached (Meters)
+        double                    m_distance_threshold;         //The distance we are allowed to be away from the waypoint to consider it reached (Meters)
 
-        double        m_time_threshold;             //How long should we wait until we establish a new waypoint If the old one hasnt been reached
+        double                    m_time_threshold;             //How long should we wait until we establish a new waypoint If the old one hasnt been reached
 
     public:
         Control_Unit();
-        bool    is_active();
 
-        bool    init(std::string destination, std::string settings);
+        bool          is_active();
+        bool          init(std::string destination, std::string settings);
 
-        void    set_waypoint(GPS_POSITION waypoint);
-        GPS_POSITION get_destination();
+        GPS_POSITION  get_destination();
+        void          set_destination(GPS_POSITION destination);
+        
+        GPS_POSITION  get_waypoint();
+        void          set_waypoint(GPS_POSITION waypoint);
 
-        void    set_destination(GPS_POSITION destination);
-        GPS_POSITION get_waypoint();
+        void          set_threshold(double value);
+        double        get_threshold();
 
-        void    set_threshold(double value);
-        double  get_threshold();
-
-        bool validate_inits(std::vector<bool> statuses);
+        bool          validate_inits(std::vector<bool> statuses);
 
 };
 #endif//__CONTROL_UNIT_HPP__
