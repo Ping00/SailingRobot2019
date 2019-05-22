1bfad57f030273e35e987bc8ba896455ebb6a420
Jonatan <jonatan.karlberg@ha.ax>
2019-05-22 09:10:46 +0300

    improving logging details to utilise tool

--- Core/Logger/logger.cpp
+++ Core/Logger/logger.cpp
@@ -14,7 +14,7 @@ Logger::Logger(std::string path)
 
 void Logger::log_data(LOG packet)
 {
-    	m_log.m_latitude = packet.m_latitude;
+    m_log.m_latitude = packet.m_latitude;
 	m_log.m_longitude = packet.m_longitude;
 	m_log.m_timestamp = packet.m_timestamp;
 
@@ -25,13 +25,13 @@ void Logger::publish()
 {
     if(m_available)
     {
-        int bearing             = m_log.m_bearing;
+		//int bearing             = m_log.m_bearing;
         double latitude         = m_log.m_latitude;
         double longitude        = m_log.m_longitude;
-        double speed            = m_log.m_speed;
+		//double speed            = m_log.m_speed;
         std::string timestamp   = m_log.m_timestamp;
-        double waypoint         = m_log.m_distance_from_waypoint;
-        double checkpoint       = m_log.m_distance_from_destination;
+		//double waypoint         = m_log.m_distance_from_waypoint;
+        //double checkpoint       = m_log.m_distance_from_destination;
 
         std::stringstream stream;
         stream << timestamp << " " << std::setprecision(15) << latitude << " " << std::setprecision(15) << longitude;
@@ -50,14 +50,26 @@ void Logger::publish()
     }
 }
 
-void Logger::publish_waypoint(GPS_DATA data, std::string message)
+void Logger::publish_waypoint(GPS_DATA from, GPS_POSITION to, std::string message)
 {
       std::string timestamp = data.get_time();
-      double lat = data.get_latitude();
-      double lon = data.get_longitude();
+      double at_lat = data.get_latitude();
+      double at_lon = data.get_longitude();
+
+	  double dest_lat = to.latitude;
+	  double dest_lon = to.longitude;
+
 
       std::stringstream stream;
-      stream << timestamp << " : " << std::setprecision(10) << lat << "," << std::setprecision(10) << lon;
+	  stream << timestamp 
+		  << " : " << "[" 
+		  << std::setprecision(10) << at_lat 
+		  << "," 
+		  << std::setprecision(10) << at_lon 
+		  << "]" << " (->) " << "["
+		  << std::setprecision(10) << dest_lat 
+		  << std::setprecision(10) << dest_lon 
+		  << "]";
       std::string output = stream.str();
 
       IO io;
