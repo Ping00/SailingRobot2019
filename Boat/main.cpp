8a53d629a06471078285c08df6fa596813f51341
Jonatan <jonatan.karlberg@ha.ax>
2019-05-22 09:47:08 +0300

    removed unneeded prints

--- Boat/main.cpp
+++ Boat/main.cpp
@@ -446,8 +446,7 @@ int main(int argc, char* argv[])
         fresh_log.m_distance_from_waypoint      = waypoint_distance;
         fresh_log.m_distance_from_destination   = checkpoint_distance;
         data_logger.log_data(fresh_log);
- 
-   std::cout << "LOG: " << fresh_log.m_latitude << std::endl;
+
 
     }
 
